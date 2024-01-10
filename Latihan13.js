aa0dadefee53f768894f94b6592512efa78c6629
abdulgits <ided.263@gmail.com>
2024-01-10 19:05:45 +0700

    horeee

--- Latihan13.js
+++ Latihan13.js
@@ -2,9 +2,9 @@ var jmlAngkot = 10;
 var angkotBeroperasi = 6;
 
 for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
-  if (noAngkot <= 6) {
+  if (noAngkot <= 6 && noAngkot == !5) {
     console.log("Angkot No. " + noAngkot + " Beroperasi dengan baik.");
-  } else if (noAngkot === 8) {
+  } else if (noAngkot === 8 || noAngkot === 10 || noAngkot === 5) {
     console.log("Angkot No. " + noAngkot + " Sedang lembur.");
   } else {
     console.log("Angkot No. " + noAngkot + " Sedang Tidak Beroperasi. ");
---
+++ Latihan14.js
@@ -0,0 +1,11 @@
+var angka = parseInt(prompt("masukkan angka : "));
+
+if (angka === 1) {
+  alert("angka yang anda masukkan adalah 1.");
+} else if (angka === 2) {
+  alert("angka yang anda masukkan adalah 2.");
+} else if (angka === 3) {
+  alert("angka yang anda masukka adalah 3.");
+} else {
+  alert("angka yang anda masukkan salah!");
+}
