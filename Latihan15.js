d11d0f872436e1297ebfc0d7c8e4e02093163795
abdulgits <ided.263@gmail.com>
2024-01-10 19:24:44 +0700

    swicth

---
+++ Latihan15.js
@@ -0,0 +1,16 @@
+var angka = prompt("masukkan angka :");
+
+switch (angka) {
+  case "1":
+    alert("Angka Yang Anda Masukkan Adalah 1");
+    break;
+  case "2":
+    alert("Angka Yang Anda Masukkan Adalah 2");
+    break;
+  case "3":
+    alert("Angka Yang Anda Masukkan Adalah 3");
+    break;
+  default:
+    alert("Angka Anda Masukkan Salah!");
+    break;
+}
